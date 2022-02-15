9a019f0562eb1ac216e0a56e6b65cecbfbbd40fd
Samir Schabel <90188949+Starkiller1988@users.noreply.github.com>
2022-02-15 11:11:37 +0100

    create a Example Project

---
+++ src/main/java/com/example/uiexample/UiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.uiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiExampleApplication.class, args);
+    }
+
+}
